282709e9b3974fddfbd7c687eea430d303521c89
MohammadEImmam <immam.m@ufl.edu>
2023-02-07 00:41:08 -0500

    product component & sample product page
    http://localhost:4200/sample_product

---
+++ web/src/app/globals/product/product.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import {Product} from '../../schema/product.schema';
+@Component({
+  templateUrl: 'product.component.html',
+  styleUrls: ['product.component.css'],
+  selector: 'product',
+})
+
+export class ProductComponent {
+  faShoppingCart = faShoppingCart;
+  @Input() product: Product;
+}
