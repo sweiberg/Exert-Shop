753e66313aed0d47521e3db29dd932ce0483a00a
Stephen Weiberg <sweiberg@ufl.edu>
2023-02-22 20:04:03 -0500

    Added user login and registration forms

--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({ templateUrl: 'login.component.html' })
-export class LoginComponent {}
+export class LoginComponent {
+    hide = true;
+
+    email = new FormControl('', [Validators.required, Validators.email]);
+
+    getErrorMessage() {
+      if (this.email.hasError('required')) {
+        return 'You must enter a value';
+      }
+  
+      return this.email.hasError('email') ? 'Not a valid email' : '';
+    }
+    
+    Submit(){
+        console.log('workin');
+      }
+}
--- web/src/app/register/register.component.ts
+++ web/src/app/register/register.component.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({ templateUrl: 'register.component.html' })
-export class RegisterComponent {}
+export class RegisterComponent {
+    hide = true;
+
+    email = new FormControl('', [Validators.required, Validators.email]);
+
+    getErrorMessage() {
+      if (this.email.hasError('required')) {
+        return 'You must enter a value';
+      }
+  
+      return this.email.hasError('email') ? 'Not a valid email' : '';
+    }
+    
+    Submit(){
+        console.log('workin');
+      }
+}
