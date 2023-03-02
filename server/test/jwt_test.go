5910312d13f8b4982086f09cc8b74745ff42387a
jweiberg <jonweiberg@gmail.com>
2023-03-01 21:10:23 -0500

    Update jwt_test.go
    Added a test for user registration.
    
    Added a test for user login.

--- server/test/jwt_test.go
+++ server/test/jwt_test.go
@@ -2,43 +2,52 @@ package test
 
 import (
 	"io"
-	"log"
 	"net/http"
 	"strings"
 	"testing"
 )
 
-func TestJWT(t *testing.T) {
+func TestCreateUser(t *testing.T) {
 	client := &http.Client{}
 
 	var data = strings.NewReader(`{"username" : "test", "email" : "test@gmail.com", "password" : "gogators"}`)
 	request, err := http.NewRequest("POST", "http://localhost:4300/auth/register", data)
 
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
+		t.Log("User creation failed to process the request.")
 	}
 
 	request.Header.Set("Content-Type", "application/json")
 	response, err := client.Do(request)
 
 	if err != nil {
-		log.Fatal(err)
-		t.Log("User creation failed during the JWT test.")
+		t.Fatal(err)
+		t.Log("User creation failed to receive a response.")
 	}
 
-	t.Log("User creation for JWT test passed.")
+	t.Log("User creation test passed.")
 
 	defer response.Body.Close()
+}
 
-	data = strings.NewReader(`{"username" : "test", "email" : "test@gmail.com", "password" : "gogators"}`)
-	request, err = http.NewRequest("POST", "http://localhost:4300/auth/login", data)
+func TestLoginUser(t *testing.T) {
+	client := &http.Client{}
 
-	request.Header.Set("Content-Type", "application/json")
-	response, err = client.Do(request)
+	var data = strings.NewReader(`{"username" : "test", "email" : "test@gmail.com", "password" : "gogators"}`)
+	request, err := http.NewRequest("POST", "http://localhost:4300/auth/login", data)
 
 	if err != nil {
-		log.Fatal(err)
-		t.Log("User login failed during the JWT test.")
+		t.Fatal(err)
+		t.Log("User login failed to process the request.")
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+	response, err := client.Do(request)
+
+	if err != nil {
+		t.Fatal(err)
+		t.Log("User login failed to receive a response.")
 	}
 
 	defer response.Body.Close()
@@ -46,10 +55,11 @@ func TestJWT(t *testing.T) {
 	bodyText, err := io.ReadAll(response.Body)
 
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
+		t.Log("User login failed because the body text does not exist")
 	}
 
-	t.Logf("%s\n", bodyText)
+	jwtString := string(bodyText[1])
 
-	t.Log("User login for JWT test passed.")
+	t.Logf("User login test passed. %s", jwtString)
 }
