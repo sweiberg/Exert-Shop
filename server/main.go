fd2e9e60609ba83265b59c67bf7c5c2c06906ac8
jweiberg <jonweiberg@gmail.com>
2023-03-28 15:34:47 -0400

    Add category model and routes.
    Created the category model that utilizes a one-to-many association with products.
    
    Created the view category route that fetches products in the defined category id.
    
    Created the add category route for the admin panel.
    
    Moves the CORS function in main to an independent middleware file.

--- server/main.go
+++ server/main.go
@@ -22,6 +22,7 @@ func loadDB() {
 	db.Connect()
 	db.Database.AutoMigrate(&model.User{})
 	db.Database.AutoMigrate(&model.Product{})
+	db.Database.AutoMigrate(&model.Category{})
 }
 
 func loadEnv() {
@@ -32,36 +33,22 @@ func loadEnv() {
 	}
 }
 
-func CORS() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
-}
-
 func loadRoutes() {
 	router := gin.Default()
-	router.Use(CORS())
+	router.Use(middleware.CORS())
 
 	authAPI := router.Group("/auth")
 	authAPI.POST("/register", controller.Register)
 	authAPI.POST("/login", controller.Login)
 
 	publicAPI := router.Group("/api")
-	publicAPI.GET("/viewproduct/:id", controller.ViewProduct)
+	publicAPI.GET("/product/:id", controller.ViewProduct)
+	publicAPI.GET("/category/:id", controller.ViewCategory)
 
 	protectedAPI := router.Group("/api")
 	protectedAPI.Use(middleware.VerifyJWT())
 	protectedAPI.POST("/addproduct", controller.AddProduct)
+	protectedAPI.POST("/addcategory", controller.AddCategory)
 
 	router.Run(":4300")
 
