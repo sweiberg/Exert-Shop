b7dadf45b0a1fe1bfd243365ce125cec0a909f1e
jweiberg <jonweiberg@gmail.com>
2023-03-29 14:38:18 -0400

    Update store_test.go
    Updated the retrieval of the JWT for unit testing. The test now decodes the response body. This was necessary because the login API was changed to provide the front end with the username and userid for caching.
    
    Refactored the cors middleware to include an access control URL and to conform to code standards.

--- server/middleware/cors.go
+++ server/middleware/cors.go
@@ -5,17 +5,17 @@ import (
 )
 
 func CORS() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+	return func(context *gin.Context) {
+		context.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+		context.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		context.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		context.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
+		if context.Request.Method == "OPTIONS" {
+			context.AbortWithStatus(204)
 			return
 		}
 
-		c.Next()
+		context.Next()
 	}
 }
